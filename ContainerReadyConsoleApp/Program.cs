bd509a46dc0e124e96b76467c1a2f5f056e43cbf
Rodrigo Vieira <mcbacker@gmail.com>
2018-11-16 17:56:05 -0200

    Init

---
+++ ContainerReadyConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ContainerReadyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello World running on {Environment.OSVersion}!");
+        }
+    }
+}
